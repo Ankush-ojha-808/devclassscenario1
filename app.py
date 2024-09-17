526625a9eba7e0569a55563f1e47f4ad3d4f6cc5
Ankush <ojhaankush808@gmail.com>
2024-09-17 13:30:03 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Example usage
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+
+    result = add(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
+    
